3b115b7430f02a5b0d7343e6e104bb42dd52fb62
TreverHibbs <treverhibbs@gmail.com>
2021-05-26 22:17:34 -0700

    Add webpack bundling

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("hello mom");
+import { select, selectAll } from 'd3-selection';
+import { hierarchy } from 'd3-hierarchy';
+
+const d3 = { select, selectAll, hierarchy }
+
+const mainSvg = d3.select("a");
+
+console.log("hello");
