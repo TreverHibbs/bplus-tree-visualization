8ee3a0a95c3c552d01c731a287ef08a25ad66397
TreverHibbs <treverhibbs@gmail.com>
2021-06-02 11:41:09 -0700

    Add initial d3 test code

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -4,10 +4,26 @@ import { hierarchy } from 'd3-hierarchy';
 
 const d3 = { select, selectAll, hierarchy }
 
-const mainSvg = d3.select("a");
+
+const btn = document.querySelector('.button-start');
+
+const greet = () => {
+  console.log("hello");
+}
+
+if(btn){
+  btn.addEventListener('click', greet);
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  const mainSvg = d3.select(".svg-main");
+  const myCircle = mainSvg.append("circle")
+    .attr("cx", 50)
+    .attr("cy", 50)
+    .attr("r", 40)
+    .attr("fill", "blue");
+});
 
 export const hello = () => {
   return false;
 }
-
-console.log("hello");
--- tests/test.ts
+++ tests/test.ts
@@ -2,7 +2,7 @@ import { hello } from '../src/ts/hello'; // this will be your custom import
 import { expect } from 'chai';
 
 describe('hello world test', () => { // the tests container
-    it('checking return', () => { // the single test
+    it('checking return', (): void => { // the single test
                 /* detect retina */
         expect(hello()).to.be.false; // Do I need to explain anything? It's like writing in English!
     });
