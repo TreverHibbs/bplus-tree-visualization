e71a2460f8d8acd3467233431b7e49569fed6120
TreverHibbs <treverhibbs@gmail.com>
2021-06-03 15:01:20 -0700

    add proof of concept for tree visualization

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,28 +1,68 @@
 import "./../css/main.scss";
+import { cssVar } from './colors';
 import { select, selectAll } from 'd3-selection';
-import { hierarchy } from 'd3-hierarchy';
+import { hierarchy, tree, HierarchyPointLink, HierarchyPointNode } from 'd3-hierarchy';
+import { linkVertical } from 'd3-shape';
 
-const d3 = { select, selectAll, hierarchy }
+const d3 = { select, selectAll, hierarchy, tree, linkVertical }
 
 
 const btn = document.querySelector('.button-start');
 
 const greet = () => {
-  console.log("hello");
+  console.log("jello?");
 }
 
-if(btn){
+if (btn) {
   btn.addEventListener('click', greet);
 }
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  const mainSvg = d3.select(".svg-main");
-  const myCircle = mainSvg.append("circle")
-    .attr("cx", 50)
-    .attr("cy", 50)
-    .attr("r", 40)
-    .attr("fill", "blue");
-});
+
+const mainSvg = d3.select(".svg-main");
+
+const family = d3.hierarchy({
+  name: "root",
+  children: [
+    { name: "child #1" },
+    {
+      name: "child #2",
+      children: [
+        { name: "grandchild #1" },
+        { name: "grandchild #2" },
+        { name: "grandchild #3" }
+      ]
+    }
+  ]
+})
+
+const btree = d3.tree<{ name: string }>();
+btree.size([100, 100]);
+const root = btree(family);
+
+const linkGenerator = d3.linkVertical<HierarchyPointLink<{ name: string }>, HierarchyPointNode<{ name: string }>>()
+  .x(d => d.x)
+  .y(d => d.y)
+
+mainSvg.append("g")
+  .attr("fill", "none")
+  .attr("stroke", cssVar('--grey'))
+  .attr("stroke-opacity", 1)
+  .attr("stroke-width", .5)
+  .selectAll("path")
+  .data(root.links())
+  .join("path")
+  .attr("d", linkGenerator);
+
+const nodeRectWidth = 10;
+mainSvg.append("g")
+  .attr("fill", cssVar('--crimson'))
+  .selectAll("rect")
+  .data(root.descendants())
+  .join("rect")
+  .attr("width", nodeRectWidth)
+  .attr("height", nodeRectWidth)
+  .attr("x", d => d.x-(nodeRectWidth/2))
+  .attr("y", d => d.y);
 
 export const hello = () => {
   return false;
